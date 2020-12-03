273fea3e4924ea648925a39b3d22c69dbf4df80b
andrey-dum <duminskiy@gmail.com>
2020-12-03 21:07:12 +0200

    Sidebar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Sidebar from './components/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widjets */}
+     
+    </div>
+  );
+}
+
+export default App;
